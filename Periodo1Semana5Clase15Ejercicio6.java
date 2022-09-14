daf73141f8975d4b2765fa90dc3b8338829f1645
Gabyyg <113482202+Gabyyg@users.noreply.github.com>
2022-09-14 17:53:09 -0600

    Update Periodo1Semana5Clase15Ejercicio6.java

--- Periodo1Semana5Clase15Ejercicio6.java
+++ Periodo1Semana5Clase15Ejercicio6.java
@@ -1,13 +1,15 @@
-package clase5ejercicio6;
-import java.util.Scanner ;
-public class Clase15Ejerciciol {
-    public static void main ( String [ ] args ) {
-        Scanner Entrada = new Scanner ( System.in ) ;
-        System.out.println ( " Ingrese la base : " ) ;
-        double base = Entrada.nextDouble ( ) ;
-        System.out.println ( " Ingrese el exponente : " ) ;
-        double exponente = Entrada.nextDouble ( ) ;
-        double resultadb = Math.pow ( base , exponente ) ;
-        System.out.println ( " El resultado es : " + resultado ) ;
+import java.util.Scanner;
+public class Main {
+  static Scanner entrada = new Scanner(System.in);
+  public static void main(String[] args) {
+
+    System.out.println ( "Ingrese la base : " ) ;
+    double base = entrada.nextDouble ( ) ;
+        
+    System.out.println ( "Ingrese el exponente : " ) ;  
+    double exponente = entrada.nextDouble ( ) ;
+        
+    double resultado = Math.pow ( base , exponente ) ;
+    System.out.println ( "El resultado es : " + resultado ) ;
     }
-}  
+} 
