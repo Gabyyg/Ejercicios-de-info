a0d65075ee2e085271fface9cb119a212f5d4d09
Gabyyg <113482202+Gabyyg@users.noreply.github.com>
2022-09-13 23:12:04 -0600

    Update Periodo1Semana5Clase14Ejercicio5.java

--- Periodo1Semana5Clase14Ejercicio5.java
+++ Periodo1Semana5Clase14Ejercicio5.java
@@ -5,33 +5,27 @@ public class Clase14Ejerciciol {
         Scanner Entrada = new Scanner ( System.in ) ;
         int reglas ;
         double nota ;
-    } }
-        System.out.println ( " Usted
-        System.out.println ( " Ingrese
+        System.out.println ( " Usted cumple las reglas de la universidad : " ) ;
+        System.out.println ( " Ingrese un numero : \ n 1. Siempre \ n 2. Aveces \ n 3. Nunca " ) ;
         reglas Entrada.nextInt ( ) ;
-        System.out.println ( " Que
+        System.out.println ( " Que nota obtuvo en el laboratorio : " ) ;
         nota = Entrada.nextDouble ( ) ;
         if ( reglas == 1 ) {
-                                  cumple las reglas de la universidad : " ) ;
-                                    un numero : \ n 1. Siempre \ n 2. Aveces \ n 3. Nunca " ) ;
+             if ( nota > = 8 & nota < = 10 ) {
+                nota = 10 ;                     
             } else {
-        }
-            if ( nota > = 8 & nota < = 10 ) {
-                nota = 10 ;
+                System.out.println ( " Nota invalida " ) ; 
             }
         } else if ( reglas == 2 ) {
-            if ( nota < 6 ) {
-                nota
-            } else if
-                nota
+            if ( nota < 6 ) { 
+                 nota = nota + 0.5;
+            } else if ( nota > 6 & nota < 8 ) {
+                nota = nota + 0.7;
             } else {
-            }
-        } else {
-                                nota obtuvo en el laboratorio : " ) ;
                 System.out.println ( " Nota invalida " ) ;
-                       nota + 0.5 ;
-                      ( nota > = 6 & nota < 8 ) {
-                       nota + 0.7 ;
-                System.out.println ( " Nota invalida " ) ;
+            }    
+        } else {        
             System.out.println ( " Usted debe cumplir las reglas " ) ;
-        System.out.println ( " La nueva nota es : " + nota ) ;      
+        }    
+        System.out.println ( " La nueva nota es : " + nota ) ;   
+    }}    
