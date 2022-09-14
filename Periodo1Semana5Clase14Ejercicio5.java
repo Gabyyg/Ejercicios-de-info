23cc8851004d4281abaefcf34348ab88f9906130
Gabyyg <113482202+Gabyyg@users.noreply.github.com>
2022-09-14 17:50:27 -0600

    Update Periodo1Semana5Clase14Ejercicio5.java

--- Periodo1Semana5Clase14Ejercicio5.java
+++ Periodo1Semana5Clase14Ejercicio5.java
@@ -1,31 +1,46 @@
-package clasel4ejerciciol ;
-import java.util.Scanner ;
-public class Clase14Ejerciciol {
-    public static void main ( String [ ] args ) {
-        Scanner Entrada = new Scanner ( System.in ) ;
-        int reglas ;
-        double nota ;
-        System.out.println ( " Usted cumple las reglas de la universidad : " ) ;
-        System.out.println ( " Ingrese un numero : \ n 1. Siempre \ n 2. Aveces \ n 3. Nunca " ) ;
-        reglas Entrada.nextInt ( ) ;
-        System.out.println ( " Que nota obtuvo en el laboratorio : " ) ;
-        nota = Entrada.nextDouble ( ) ;
+import java.util.Scanner;
+public class Main {
+  static Scanner entrada = new Scanner(System.in);
+  public static void main(String[] args) {
+
+      int reglas ;
+      double nota ;
+    
+      System.out.println ( "Usted cumple las reglas de la universidad : " ); 
+          
+      System.out.println ( "Ingrese un numero :") ;
+      System.out.println ( "\n1. Siempre " ); 
+      System.out.println ( "\n2. Aveces " ); 
+      System.out.println ( "\n3. Nunca " ); 
+      reglas = entrada.nextInt ( ) ;
+    
+        System.out.println ( "Que nota obtuvo en el laboratorio : " ) ;
+        nota = entrada.nextDouble ( ) ;
         if ( reglas == 1 ) {
-             if ( nota > = 8 & nota < = 10 ) {
+             
+          if ( nota >= 8 & nota <= 10 ) {
                 nota = 10 ;                     
-            } else {
-                System.out.println ( " Nota invalida " ) ; 
-            }
-        } else if ( reglas == 2 ) {
-            if ( nota < 6 ) { 
-                 nota = nota + 0.5;
-            } else if ( nota > 6 & nota < 8 ) {
-                nota = nota + 0.7;
-            } else {
-                System.out.println ( " Nota invalida " ) ;
-            }    
-        } else {        
-            System.out.println ( " Usted debe cumplir las reglas " ) ;
+              }
+          else {
+                System.out.println ( "Se mantiene la misma nota " ) ;
+                }
+        } 
+        else if ( reglas == 2 ) {
+              
+          if ( nota < 6 && nota>0) { 
+            nota = nota + 0.5;
+          }
+          else if ( nota >= 6 & nota < 8 ) {
+            nota = nota + 0.7;
+          }
+          else {
+            System.out.println ( "Se mantiene la misma nota " ) ;
+          }
+                    
+        } 
+        else {        
+            System.out.println ( "Usted debe cumplir las reglas " ) ;
         }    
-        System.out.println ( " La nueva nota es : " + nota ) ;   
-    }}    
+        System.out.println ( "La nueva nota es : " + nota ) ;   
+    }
+}     
