effd3e9caf6127875a8c773995747b5fe8506c43
Gabyyg <113482202+Gabyyg@users.noreply.github.com>
2022-09-14 17:57:08 -0600

    Update Periodo1Semana6Clase17Ejercicio7.java

--- Periodo1Semana6Clase17Ejercicio7.java
+++ Periodo1Semana6Clase17Ejercicio7.java
@@ -1,18 +1,22 @@
-package usuarioclave ;
-import java.util.Scanner ;
-public class UsuarioClave (
-    public static void main ( String [ ] args ) {
-    String nombre , clave ;
-    Scanner entrada = new Scanner ( System.in ) ;
+import java.util.Scanner;
+public class Main {
+  static Scanner entrada = new Scanner(System.in);
+  public static void main(String[] args) {
+
+    String nombre , pass ;
+    
     System.out.print ( " Ingrese Nombre de usuario : " ) ;
     nombre = entrada.nextLine();
-    System.out.print ( " Ingrese Clave de usuario : " ) ;
-    clave entrada.nextLine ( ) ;
-    if ( nombre.equals ( " juan " ) && clave.equals ( " 123456 " ) ) {
-    System.out.println ( " Bienvenido al Sistema " ) ;
+    
+    System.out.print ( " Ingrese contraseña de usuario : " ) ;
+    pass = entrada.nextLine ( ) ;
+    
+    if ( nombre.equals ( "Juan" ) && pass.equals ( "123456" ) ) {
+      System.out.println ( " Bienvenido al Sistema " ) ;
     }
     else {
-    System.out.println ( " Nombre de Usuario o contraseña Incorrecto " ) ;
+      System.out.println ( " Nombre de Usuario o contraseña Incorrecto " ) ;
     }
-    }
-}  
+    
+  }
+} 
