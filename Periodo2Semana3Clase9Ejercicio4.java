7858b36dd9ebdd03bb6fd6754ce4c4c2b1bc4335
Gabyyg <113482202+Gabyyg@users.noreply.github.com>
2022-09-14 00:34:43 -0600

    Create Periodo2Semana3Clase9Ejercicio4.java

---
+++ Periodo2Semana3Clase9Ejercicio4.java
@@ -0,0 +1,7 @@
+package periodo2clase91 ;
+public class Periodo2Clase91 (
+    public static void main ( String [ ] args ) {
+        for(int i = 100 ; i> = 1; i--)
+            System.out.println ( i ) ;
+    }  
+}    
