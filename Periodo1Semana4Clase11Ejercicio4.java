ffeabd3104636a16160901b8349c9389168628ba
Gabyyg <113482202+Gabyyg@users.noreply.github.com>
2022-09-14 17:29:52 -0600

    Update Periodo1Semana4Clase11Ejercicio4.java

--- Periodo1Semana4Clase11Ejercicio4.java
+++ Periodo1Semana4Clase11Ejercicio4.java
@@ -1,20 +1,20 @@
-package clasell.pkgl ;
-import java.util.Scanner ;
-public class Clasel11 (
-    public static void main ( String [ ] args ) {
-        Scanner Entrada = new Scanner ( System.in ) ;
+import java.util.Scanner;
+public class Main {
+  static Scanner entrada = new Scanner(System.in);
+  public static void main(String[] args) {
+
         float notal , nota2 , nota3 , promedio ;
         System.out.println ( " Ingrese la primera nota " ) ;
-        notal = Entrada.next Float ( ) ;
-        System.out.println ( " Ingrese la primera nota " ) ;
-        nota2 = Entrada.nextFloat ( ) ;
-        System.out.println ( " Ingrese la primera nota " ) ;
-        nota3 = Entrada.nextFloat ( ) ;
+        notal = entrada.nextFloat ( ) ;
+        System.out.println ( " Ingrese la segunda nota " ) ;
+        nota2 = entrada.nextFloat ( ) ;
+        System.out.println ( " Ingrese la tercera nota " ) ;
+        nota3 = entrada.nextFloat ( ) ;
         promedio = ( notal + nota2 + nota3 ) / 3 ;
         System.out.println ( " El promedio es : " + promedio ) ;
-        if ( promedio > = 7 ) {
+        if ( promedio >= 7 ) {
             System.out.println ( " Promocionado " ) ;
-        } else if ( promedio > = 4 ) {
+        } else if ( promedio >= 4 ) {
             System.out.println ( " Regular " ) ;
         } else {
             System.out.println ( " Reprobado " ) ;
