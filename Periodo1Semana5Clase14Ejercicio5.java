bfc8bb436139e7c3d4951b564a540198acefadeb
Gabyyg <113482202+Gabyyg@users.noreply.github.com>
2022-09-13 22:52:10 -0600

    Create Periodo1Semana5Clase14Ejercicio5.java

---
+++ Periodo1Semana5Clase14Ejercicio5.java
@@ -0,0 +1,37 @@
+package clasel4ejerciciol ;
+import java.util.Scanner ;
+public class Clase14Ejerciciol {
+    public static void main ( String [ ] args ) {
+        Scanner Entrada = new Scanner ( System.in ) ;
+        int reglas ;
+        double nota ;
+    } }
+        System.out.println ( " Usted
+        System.out.println ( " Ingrese
+        reglas Entrada.nextInt ( ) ;
+        System.out.println ( " Que
+        nota = Entrada.nextDouble ( ) ;
+        if ( reglas == 1 ) {
+                                  cumple las reglas de la universidad : " ) ;
+                                    un numero : \ n 1. Siempre \ n 2. Aveces \ n 3. Nunca " ) ;
+            } else {
+        }
+            if ( nota > = 8 & nota < = 10 ) {
+                nota = 10 ;
+            }
+        } else if ( reglas == 2 ) {
+            if ( nota < 6 ) {
+                nota
+            } else if
+                nota
+            } else {
+            }
+        } else {
+                                nota obtuvo en el laboratorio : " ) ;
+                System.out.println ( " Nota invalida " ) ;
+                       nota + 0.5 ;
+                      ( nota > = 6 & nota < 8 ) {
+                       nota + 0.7 ;
+                System.out.println ( " Nota invalida " ) ;
+            System.out.println ( " Usted debe cumplir las reglas " ) ;
+        System.out.println ( " La nueva nota es : " + nota ) ;      
