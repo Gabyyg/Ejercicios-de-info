608a16b8e7912d0402d1ef0eed7ca6aa71c1d669
Gabyyg <113482202+Gabyyg@users.noreply.github.com>
2022-09-14 12:57:39 -0600

    Create Periodo3Semana5Clase13Ejercicio7.java

---
+++ Periodo3Semana5Clase13Ejercicio7.java
@@ -0,0 +1,21 @@
+package periodo3clase13 ;
+import java.util.Scanner ;
+public class Periodo3Clase13 (
+    public static void main ( String [ ] args ) {
+        Scanner datos = new Scanner ( System.in ) ;
+        int valor ;
+        do {
+            System.out.print ( " Ingrese un valor entre 0 y 999 ( 0 finaliza ) : " ) ;
+            valor datos.nextInt ( ) ;
+            if ( valor > = 100 ) {
+                System.out.println ( " Es de 3 dígitos . " ) ;
+            } else {
+                if ( valor > = 10 ) {
+                    System.out.println ( " Es de 2 dígitos . " ) ;
+                } else {
+                    System.out.println ( " Es de 1 dígito . " ) ;
+                }
+            }  
+        } while ( valor ! = 0 ) ;
+    }
+}  
