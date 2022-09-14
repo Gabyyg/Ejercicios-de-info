5dc045db475fda6e639c9985238c2441a6515c07
Gabyyg <113482202+Gabyyg@users.noreply.github.com>
2022-09-14 12:02:55 -0600

    Create Periodo3Semana1Clase3Ejercicio1.java

---
+++ Periodo3Semana1Clase3Ejercicio1.java
@@ -0,0 +1,15 @@
+public class swpropuestol {
+    public static void main ( String [ ] args ) 
+        Scanner ingresar - new Scanner ( System.in ) ;
+        double nota ;
+  
+        System.out.println ( " favor ingresar un valor desde 1 hasta 10 " ) ;
+        nota = ingresar.nextDouble ( ) ;
+  
+        while ( nota < 1 | nota > 10 )
+        {
+        System.out.println ( " Lo siento mucho pero el valor proporcionado no es correcto " ) ;
+        nota = ingresar.nextDouble ( ) ;
+        }
+    }
+}
