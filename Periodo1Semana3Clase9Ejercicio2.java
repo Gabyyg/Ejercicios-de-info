463e8acdbd880008b8102e92a2db220eac97cff9
Gabyyg <113482202+Gabyyg@users.noreply.github.com>
2022-09-14 13:12:28 -0600

    Update Periodo1Semana3Clase9Ejercicio2.java

--- Periodo1Semana3Clase9Ejercicio2.java
+++ Periodo1Semana3Clase9Ejercicio2.java
@@ -1,35 +1,39 @@
-package comparacion ;
-import java.util.Scanner ;
-public class Comparacion {
-     public static void main ( String [ ] args ) {
-           
-            Scanner entrada = new Scanner ( System.in ) ;
-                       
-            int numerol ;
+import java.util.Scanner;
+public class Main {
+  static Scanner entrada = new Scanner(System.in);
+  public static void main(String[] args) {                       
+            int numero1 ;
             int numero2 ; 
                        
             System.out.print ( " Escriba el primer entero : " ) ;
-            numerol = entrada.nextInt ( ) ; 
+            numero1 = entrada.nextInt ( ) ; 
                        
             System.out.print ( " Escriba el segundo entero : " ) ;
             numero2 = entrada.nextInt () ;
                        
-            if ( numerol == numero2 )
-            System.out.println ( " El numero" + numerol + " es igual a " + numero2);
-                       
-            if ( numerol ! = numero2 )
-            System.out.println ( " El numero " + numerol + " es distinto a " + numero2);
-                       
-            if ( numerol < numero2 )
-            System.out.println ( " El numero " + numerol + " es menor a " + numero2);
-                       
-            if ( numerol > numero2 )
-            System.out.println ( " El numero " + numerol + " es mayor a " + numero2);
-                       
-            if ( numerol < = numero2 )
-            System.out.println ( " El numero " + numerol + " es menor o igual a" + numero2);
-                       
-            if ( numerol > = numero2 )   
-            System.out.println ( " El numero " + numerol + " es mayor o igual a " + numero2);
+            if ( numero1 == numero2 ) {
+              System.out.println ( " El numero" + numero1 + " es igual a " + numero2);
+            }
+                    
+            else if ( numero1 != numero2 ){
+              System.out.println ( " El numero " + numero1 + " es distinto a " + numero2);
+            }
+         
+            else if ( numero1 < numero2 ){
+             System.out.println ( " El numero " + numero1 + " es menor a " + numero2); 
+            }
+                                   
+            else if ( numero1 > numero2 ){
+            System.out.println ( " El numero " + numero1 + " es mayor a " + numero2);
+            }
+                                   
+            else if ( numero1 <= numero2 ){
+            System.out.println ( " El numero " + numero1 + " es menor o igual a" + numero2);
+            }
+                               
+           else if ( numero1 >= numero2 ){
+            System.out.println ( " El numero " + numero1 + " es mayor o igual a " + numero2);
+           }
+           
      }                  
-}    
+}       
