e60d11246a576067dcc8b047475959540811a942
Gabyyg <113482202+Gabyyg@users.noreply.github.com>
2022-09-14 18:13:27 -0600

    Update Periodo2Semana2Clase6Ejercicio3.java

--- Periodo2Semana2Clase6Ejercicio3.java
+++ Periodo2Semana2Clase6Ejercicio3.java
@@ -1,20 +1,22 @@
-package periodo2clase6 ;
-public class Periodo2Clase6 {
-    public static void main ( String [ ] args ) {
-        String str = " dos " ;
+import java.util.Scanner;
+public class Main {
+  static Scanner entrada = new Scanner(System.in);
+  public static void main(String[] args) {
+
+        String str = "dos" ;
         switch ( str )
         {
-            case " uno " :
+            case "uno" :
                 System.out.println ( " uno " ) ;
                 break ;
-            case " dos " :
+            case "dos" :
                 System.out.println ( " dos " ) ;
                 break ;
-            case " tres " :
+            case "tres" :
                 System.out.println ( " tres " ) ;
                 break ;
             default :
                 System.out.println ( " no coincide " ) ;
         }
     }
-}  
+} 
