e0e1105458fca18cf6e66bb98de8421e2982b194
Gabyyg <113482202+Gabyyg@users.noreply.github.com>
2022-09-15 22:59:09 -0600

    Update Periodo3Semana3Clase7Ejercicio4.java

--- Periodo3Semana3Clase7Ejercicio4.java
+++ Periodo3Semana3Clase7Ejercicio4.java
@@ -1,11 +1,12 @@
-package periodo3clase7 ;
-public class Periodo3Clase7 {
-    public static void main ( String [ ] args ) {
-        int i = 0 ;
+public class Main {
+  public static void main(String[] args) {
+    
+       int i = 0 ;
         while ( true ) {
-            i ++ ;
+            i++ ;
             System.out.println ( " Valor de i : " + i ) ;
-            if ( i==9 ) { break ; }
+            if ( i==9 ) 
+              { break ; }
         }
     }
-}  
+} 
