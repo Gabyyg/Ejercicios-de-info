8c16abcb85cc4a1f958dbb0b1cab1e533bc37dd7
Gabyyg <113482202+Gabyyg@users.noreply.github.com>
2022-09-15 22:44:42 -0600

    Update Periodo3Semana1Clase3Ejercicio1.java

--- Periodo3Semana1Clase3Ejercicio1.java
+++ Periodo3Semana1Clase3Ejercicio1.java
@@ -1,14 +1,16 @@
-public class swpropuestol {
-    public static void main ( String [ ] args ) 
-        Scanner ingresar - new Scanner ( System.in ) ;
+import java.util.Scanner;
+public class Main {
+  static Scanner ingresar = new Scanner(System.in);
+  public static void main(String[] args) {
+    
         double nota ;
   
         System.out.println ( " favor ingresar un valor desde 1 hasta 10 " ) ;
         nota = ingresar.nextDouble ( ) ;
   
-        while ( nota < 1 | nota > 10 )
+        while ( nota < 1 || nota > 10 )
         {
-        System.out.println ( " Lo siento mucho pero el valor proporcionado no es correcto " ) ;
+        System.out.println ( " Lo siento mucho pero el valor proporcionado no es correcto, vuelva a ingesar el numero " ) ;
         nota = ingresar.nextDouble ( ) ;
         }
     }
