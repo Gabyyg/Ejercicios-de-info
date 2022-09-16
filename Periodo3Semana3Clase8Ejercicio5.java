c790f301f3c86a41edf6f9d9a0f2c6b1b497c3d5
Gabyyg <113482202+Gabyyg@users.noreply.github.com>
2022-09-15 23:06:45 -0600

    Update Periodo3Semana3Clase8Ejercicio5.java

--- Periodo3Semana3Clase8Ejercicio5.java
+++ Periodo3Semana3Clase8Ejercicio5.java
@@ -1,12 +1,12 @@
-package periodo3clases8 ;
-import java.util.Scanner ;
-public class Periodo3Clase8 {
-    public static void main ( String [ ] args ) {
-        Scanner Reader = new Scanner ( System.in ) ;
-        String quieroJugar = " si " ;
-        while ( quieroJugar.equals ( " si " ) ) {
-        System.out.println ( " ¿ Quieres seguir jugando ? " ) ;
-        quieroJugar = Reader.next ( ) ;
+import java.util.Scanner;
+public class Main {
+  static Scanner entrada = new Scanner(System.in);
+  public static void main(String[] args) {
+        
+        String quieroJugar = "si" ;
+        while ( quieroJugar.equals ( "si" ) ) {
+        System.out.println ( "¿Quieres seguir jugando?" ) ;
+        quieroJugar = entrada.next ( ) ;
         }
     }
 }  
